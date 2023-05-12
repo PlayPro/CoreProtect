09bc8466647e25b6c938d65bfac6bd20d1e205e9
violetc <58360096+s-yh-china@users.noreply.github.com>
2023-05-13 02:42:06 +0800

    Use whoPlacedCache

--- src/main/java/net/coreprotect/consumer/process/StructureGrowthProcess.java
+++ src/main/java/net/coreprotect/consumer/process/StructureGrowthProcess.java
@@ -21,7 +21,7 @@ class StructureGrowthProcess {
             Map<Integer, List<BlockState>> blockLists = Consumer.consumerBlockList.get(processId);
             if (blockLists.get(id) != null) {
                 List<BlockState> blockStates = blockLists.get(id);
-                String resultData = Lookup.whoPlaced(statement, block);
+                String resultData = Lookup.whoPlacedCache(block);
                 if (resultData.length() > 0) {
                     user = resultData;
                 }

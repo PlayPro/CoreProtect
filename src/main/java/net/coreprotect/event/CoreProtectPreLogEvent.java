ea290b76a3635f93b59d31291ab12409515cff0a
takejohn <105504345+takejohn@users.noreply.github.com>
2023-08-14 17:51:59 +0900

    Add clause "implements Cancellable"

--- src/main/java/net/coreprotect/event/CoreProtectPreLogEvent.java
+++ src/main/java/net/coreprotect/event/CoreProtectPreLogEvent.java
@@ -1,9 +1,10 @@
 package net.coreprotect.event;
 
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class CoreProtectPreLogEvent extends Event {
+public class CoreProtectPreLogEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
     private boolean cancelled = false;
@@ -18,10 +19,12 @@ public class CoreProtectPreLogEvent extends Event {
         return user;
     }
 
+    @Override
     public boolean isCancelled() {
         return cancelled;
     }
 
+    @Override
     public void setCancelled(boolean cancel) {
         this.cancelled = cancel;
     }

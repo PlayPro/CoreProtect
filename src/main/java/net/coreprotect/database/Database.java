ba6635578613b38b2bba6421f51dbfb3d5047ab1
Karl <KarltroidContact@Gmail.com>
2024-05-04 22:28:53 -0400

    Removed shulker check so shulker contents get logged as removed when destroyed

--- src/main/java/net/coreprotect/database/Database.java
+++ src/main/java/net/coreprotect/database/Database.java
@@ -111,7 +111,7 @@ public class Database extends Queue {
     }
 
     public static void containerBreakCheck(String user, Material type, Object container, ItemStack[] contents, Location location) {
-        if (BlockGroup.CONTAINERS.contains(type) && !BlockGroup.SHULKER_BOXES.contains(type)) {
+        if (BlockGroup.CONTAINERS.contains(type)) {
             if (Config.getConfig(location.getWorld()).ITEM_TRANSACTIONS) {
                 try {
                     if (contents == null) {

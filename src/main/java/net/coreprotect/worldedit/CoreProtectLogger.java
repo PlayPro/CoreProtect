f8a8e3f657e93c0ee60fa0b91154a12e5d7a7938
Intelli <contact@intelli.software>
2024-05-12 11:47:25 -0600

    Cleanup

--- src/main/java/net/coreprotect/worldedit/CoreProtectLogger.java
+++ src/main/java/net/coreprotect/worldedit/CoreProtectLogger.java
@@ -1,5 +1,9 @@
 package net.coreprotect.worldedit;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 import com.sk89q.worldedit.MaxChangedBlocksException;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
@@ -14,20 +18,16 @@ import com.sk89q.worldedit.world.World;
 import com.sk89q.worldedit.world.block.BaseBlock;
 import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
+
 import net.coreprotect.config.Config;
 import net.coreprotect.utility.Util;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
-public class CoreProtectLogger extends AbstractDelegateExtent
-{
+public class CoreProtectLogger extends AbstractDelegateExtent {
     private final Actor eventActor;
     private final World eventWorld;
     private final Extent eventExtent;
 
-    protected CoreProtectLogger(Actor actor, World world, Extent extent)
-    {
+    protected CoreProtectLogger(Actor actor, World world, Extent extent) {
         super(extent);
         this.eventActor = actor;
         this.eventWorld = world;
@@ -35,17 +35,13 @@ public class CoreProtectLogger extends AbstractDelegateExtent
     }
 
     @Override
-    public <T extends BlockStateHolder<T>> boolean setBlock(BlockVector3 position, T block) throws WorldEditException
-    {
+    public <T extends BlockStateHolder<T>> boolean setBlock(BlockVector3 position, T block) throws WorldEditException {
         org.bukkit.World world = BukkitAdapter.adapt(eventWorld);
-        if (!Config.getConfig(world).WORLDEDIT)
-        {
-            if (CoreProtectEditSessionEvent.isFAWE())
-            {
+        if (!Config.getConfig(world).WORLDEDIT) {
+            if (CoreProtectEditSessionEvent.isFAWE()) {
                 return eventExtent.setBlock(position.getX(), position.getY(), position.getZ(), block);
             }
-            else
-            {
+            else {
                 return eventExtent.setBlock(position, block);
             }
         }
@@ -60,18 +56,14 @@ public class CoreProtectLogger extends AbstractDelegateExtent
         // e.g. BaseBlock block = eventWorld.getBlock(position);
         ItemStack[] containerData = CoreProtectEditSessionEvent.isFAWE() ? null : Util.getContainerContents(oldType, null, location);
 
-        if (CoreProtectEditSessionEvent.isFAWE())
-        {
-            if (eventExtent.setBlock(position.getX(), position.getY(), position.getZ(), block))
-            {
+        if (CoreProtectEditSessionEvent.isFAWE()) {
+            if (eventExtent.setBlock(position.getX(), position.getY(), position.getZ(), block)) {
                 WorldEditLogger.postProcess(eventExtent, eventActor, position, location, block, baseBlock, oldType, oldBlock, containerData);
                 return true;
             }
         }
-        else
-        {
-            if (eventExtent.setBlock(position, block))
-            {
+        else {
+            if (eventExtent.setBlock(position, block)) {
                 WorldEditLogger.postProcess(eventExtent, eventActor, position, location, block, baseBlock, oldType, oldBlock, containerData);
                 return true;
             }
@@ -81,17 +73,14 @@ public class CoreProtectLogger extends AbstractDelegateExtent
     }
 
     @Override
-    public <T extends BlockStateHolder<T>> boolean setBlock(int x, int y, int z, T block) throws WorldEditException
-    {
+    public <T extends BlockStateHolder<T>> boolean setBlock(int x, int y, int z, T block) throws WorldEditException {
         return this.setBlock(BlockVector3.at(x, y, z), block);
     }
 
     @Override
-    public int replaceBlocks(final Region region, final Mask mask, final Pattern pattern) throws MaxChangedBlocksException
-    {
+    public int replaceBlocks(final Region region, final Mask mask, final Pattern pattern) throws MaxChangedBlocksException {
         org.bukkit.World world = BukkitAdapter.adapt(eventWorld);
-        if (!Config.getConfig(world).WORLDEDIT)
-        {
+        if (!Config.getConfig(world).WORLDEDIT) {
             return eventExtent.replaceBlocks(region, mask, pattern);
         }
         processPatternToBlocks(world, region, pattern);
@@ -99,21 +88,17 @@ public class CoreProtectLogger extends AbstractDelegateExtent
     }
 
     @Override
-    public int setBlocks(Region region, Pattern pattern) throws MaxChangedBlocksException
-    {
+    public int setBlocks(Region region, Pattern pattern) throws MaxChangedBlocksException {
         org.bukkit.World world = BukkitAdapter.adapt(eventWorld);
-        if (!Config.getConfig(world).WORLDEDIT)
-        {
+        if (!Config.getConfig(world).WORLDEDIT) {
             return eventExtent.setBlocks(region, pattern);
         }
         processPatternToBlocks(world, region, pattern);
         return eventExtent.setBlocks(region, pattern);
     }
 
-    private void processPatternToBlocks(org.bukkit.World world, Region region, Pattern pattern)
-    {
-        for (BlockVector3 position : region.clone())
-        {
+    private void processPatternToBlocks(org.bukkit.World world, Region region, Pattern pattern) {
+        for (BlockVector3 position : region.clone()) {
             BlockState oldBlock = eventExtent.getBlock(position);
             Material oldType = BukkitAdapter.adapt(oldBlock.getBlockType());
             Location location = new Location(world, position.getBlockX(), position.getBlockY(), position.getBlockZ());

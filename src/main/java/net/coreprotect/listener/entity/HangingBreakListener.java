9958d372e32d270acbf90026ceb8b209dac50c0f
Intelli <contact@intelli.software>
2024-11-05 10:17:55 -0700

    Fixed IncompatibleClassChangeError when breaking paintings in 1.21.1 and earlier

--- src/main/java/net/coreprotect/listener/entity/HangingBreakListener.java
+++ src/main/java/net/coreprotect/listener/entity/HangingBreakListener.java
@@ -72,7 +72,12 @@ public final class HangingBreakListener extends Queue implements Listener {
                     material = Material.PAINTING;
                     Painting painting = (Painting) entity;
                     blockData = "FACING=" + painting.getFacing().name();
-                    itemData = Util.getArtId(painting.getArt().toString(), true);
+                    try {
+                        itemData = Util.getArtId(painting.getArt().toString(), true);
+                    }
+                    catch (IncompatibleClassChangeError e) {
+                        // 1.21.2+
+                    }
                 }
 
                 if (!event.isCancelled() && Config.getConfig(blockEvent.getWorld()).NATURAL_BREAK) {

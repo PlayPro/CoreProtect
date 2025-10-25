f090b5a45dcdfa3ff83b5c536aa2b210a0f586f0
Leonard Steppy <leonard.steppy@outlook.de>
2025-10-25 18:06:13 +0200

    Improve performance of givable items methods

--- src/main/java/net/coreprotect/utility/ItemUtils.java
+++ src/main/java/net/coreprotect/utility/ItemUtils.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;

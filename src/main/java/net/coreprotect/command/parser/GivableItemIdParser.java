a145179b00b28369beb23b4049e1052cc3388871
Leonard Steppy <leonard.steppy@outlook.de>
2025-10-25 22:20:15 +0200

    Fix itemId parsing

--- src/main/java/net/coreprotect/command/parser/GivableItemIdParser.java
+++ src/main/java/net/coreprotect/command/parser/GivableItemIdParser.java
@@ -8,8 +8,8 @@ public class GivableItemIdParser {
     protected static final Pattern PATTERN = Pattern.compile("#([0-9]+)");
 
     public static Integer parseGivableItemId(String[] inputArguments) {
-        if (inputArguments.length > 0) {
-            Matcher matcher = PATTERN.matcher(inputArguments[0]);
+        for (String argument : inputArguments) {
+            Matcher matcher = PATTERN.matcher(argument);
             if (matcher.find()) {
                 return Integer.parseInt(matcher.group(1));
             }

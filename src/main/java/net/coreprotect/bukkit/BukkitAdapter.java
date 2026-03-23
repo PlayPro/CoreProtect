dc4e9f8d60e161352aea6061bfdce0c466496266
Alisa Gusmeroli <96133902+guss-alberto@users.noreply.github.com>
2026-03-23 21:58:12 +0100

    Prevent logging of wind burst explosions and improve BlockExplodeListener (fixes #830)

--- src/main/java/net/coreprotect/bukkit/BukkitAdapter.java
+++ src/main/java/net/coreprotect/bukkit/BukkitAdapter.java
@@ -22,6 +22,8 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -314,6 +316,17 @@ public class BukkitAdapter implements BukkitInterface {
     @Override
     public boolean isWaxed(Sign sign) {
         return false;
+    }
+
+    @Override
+    public boolean shouldLogExplosion(Event event){
+        return true;
+    }
+
+    @Override
+    public Material getExplodedBlock(BlockExplodeEvent event){
+        // accoding to the Bukkit docs this will always return air
+        return event.getBlock().getType();
     }
 
     @Override

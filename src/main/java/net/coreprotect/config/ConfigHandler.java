0fbf7d569ce447d0ccdda109cab4dd29e10727a7
CubeBeveled <cubebeveled@gmail.com>
2026-05-11 15:04:35 +0200

    updated to 26.1.2

--- src/main/java/net/coreprotect/config/ConfigHandler.java
+++ src/main/java/net/coreprotect/config/ConfigHandler.java
@@ -55,7 +55,7 @@ public class ConfigHandler extends Queue {
     public static final String JAVA_VERSION = "11.0";
     public static final String MINECRAFT_VERSION = "1.16";
     public static final String PATCH_VERSION = "23.2";
-    public static final String LATEST_VERSION = "26.1";
+    public static final String LATEST_VERSION = "26.1.2";
     public static String path = "plugins/CoreProtect/";
     public static String sqlite = "database.db";
     public static String host = "127.0.0.1";

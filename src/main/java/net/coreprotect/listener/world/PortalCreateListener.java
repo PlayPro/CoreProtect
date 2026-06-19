c9bd4bfee906809acb28e730180d5fbb18eed21a
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-06-19 12:59:16 +0200

    Improve portal creation attribution
    A brand new portal being placed due to nether pairing or the obsidian platform in the end being placed will now be logged with the proper user instead of #portal

--- src/main/java/net/coreprotect/listener/world/PortalCreateListener.java
+++ src/main/java/net/coreprotect/listener/world/PortalCreateListener.java
@@ -1,8 +1,13 @@
 package net.coreprotect.listener.world;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.BlockState;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -13,12 +18,15 @@ import net.coreprotect.consumer.Queue;
 import net.coreprotect.database.Lookup;
 import net.coreprotect.utility.BlockUtils;
 
+import java.util.Locale;
+import java.util.UUID;
+
 public final class PortalCreateListener extends Queue implements Listener {
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    protected void onPortalCreate(PortalCreateEvent event) {
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPortalCreate(PortalCreateEvent event) {
         World world = event.getWorld();
-        if (event.isCancelled() || !Config.getConfig(world).PORTALS) {
+        if (!Config.getConfig(world).PORTALS) {
             return;
         }
 
@@ -31,6 +39,34 @@ public final class PortalCreateListener extends Queue implements Listener {
                     user = resultData;
                     break;
                 }
+            }
+        }
+
+        if (user.equals("#portal")) {
+            // Find a more specific user, the check above only works for the fire creation cause, not the nether pair or end platform ones.
+
+            final Entity causingEntity = switch (event.getEntity()) {
+                case Player p -> p;
+                case Projectile projectile when projectile.getShooter() instanceof Entity entity ->
+                        entity;
+                case Item item -> {
+                    final UUID thrower = item.getThrower();
+                    if (thrower != null) {
+                        if (Bukkit.getServer().getPlayer(thrower) instanceof Player player) {
+                            yield player;
+                        } else if (event.getWorld().getEntity(thrower) instanceof Entity entity) {
+                            yield entity;
+                        }
+                    }
+                    yield event.getEntity();
+                }
+                case null, default -> event.getEntity();
+            };
+
+            if (causingEntity instanceof Player player) {
+                user = player.getName();
+            } else if (causingEntity != null) {
+                user = "#" + causingEntity.getType().name().toLowerCase(Locale.ROOT);
             }
         }
 

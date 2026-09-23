78fbf12d9e5b7cc08e9c38bcb345c060573e5ddd
Intelli <contact@intelli.software>
2026-09-23 14:37:50 -0600

    CoreProtect Community Edition v24.1

--- src/main/java/net/coreprotect/config/ConfigHandler.java
+++ src/main/java/net/coreprotect/config/ConfigHandler.java
@@ -84,7 +84,7 @@ public class ConfigHandler extends Queue {
     public static final String COMMUNITY_EDITION = "Community Edition";
     public static final String JAVA_VERSION = "11.0";
     public static final String MINECRAFT_VERSION = "1.16.5";
-    public static final String PATCH_VERSION = "24.0";
+    public static final String PATCH_VERSION = "24.1";
     public static final String LATEST_VERSION = "26.2";
     private static final String DEFAULT_SQLITE_DATABASE = "database.db";
     public static String path = "plugins/CoreProtect/";
